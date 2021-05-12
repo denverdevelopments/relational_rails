cf61b5b20158c67b591d8a93377e25f3997c10c8
kevn7n <kevn_nguyen@yahoo.com>
2021-05-11 21:05:13 -0600

    Create spec model tests for shop, candy

--- spec/features/candies/show_spec.rb
+++ spec/features/candies/show_spec.rb
@@ -8,8 +8,8 @@ RSpec.describe 'the candy show page', type: :feature do
 
     expect(page).to have_content("CANDY ##{@candy_1.id}")
     expect(page).to have_content(@candy_1.type)
-    expect(page).to have_content("All Natural? #{@candy_1.all_natural}")
-    expect(page).to have_content("How many calories? #{@candy_1.calories}")
+    expect(page).to have_content("All Natural? --  #{@candy_1.all_natural}")
+    expect(page).to have_content("How many calories? -- #{@candy_1.calories} cal")
     # expect(page).to have_link("Edit Discount")
     #   click_link "Edit Discount"
     # expect(current_path).to eq("/merchants/#{merchant_1.id}/discounts/#{discount_1.id}/edit")
