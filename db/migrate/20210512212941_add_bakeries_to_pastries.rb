20bb17daa5af8b6f6a20cd2c194fdf4588f61bfb
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-05-12 14:42:03 -0700

    Finish seeds file

--- db/migrate/20210512212941_add_bakeries_to_pastries.rb
+++ db/migrate/20210512212941_add_bakeries_to_pastries.rb
@@ -1,5 +1,5 @@
 class AddBakeriesToPastries < ActiveRecord::Migration[5.2]
   def change
-    add_reference :pastries, :bakeries, foreign_key: true
+    add_reference :pastries, :bakery, foreign_key: true
   end
 end
