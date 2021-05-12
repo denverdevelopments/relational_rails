a0cab462a3bbd1e0bd59e37401eb8a8560d11e8d
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-05-11 20:21:13 -0700

    Create route and index.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get '/', to: 'welcome#index'
-  
+
   ## Leigh's Space
   get '/bakeries', to: 'bakeries#index'
   get '/bakeries/:id', to: 'bakeries#show'
+
+  get '/pastries', to: 'pastries#index'
+  get '/pastries/:id', to: 'pastries#show'
 
   ## Kevin's space
   get '/shops', to: 'shops#index'
