3c5bb02610178a73419dfcac3f47e3884221f52f
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-05-12 21:03:46 -0700

    Create feature test folders and spec files

---
+++ spec/features/bakeries/show_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe 'the bakery show page', type: :feature do
+  before(:each) do
+    @bakery_1 = Bakery.create!(name: "Black Dalia Flour Bakery", open_on_weekends: false, hours_open: 8)
+    @bakery_2 = Bakery.create!(name: "The Hawk Spot Bakery", open_on_weekends: true, hours_open: 6)
+  end
+
+  it 'shows the bakery name and attributes' do
+    visit "/bakeries/#{@bakery_1.id}"
+
+    expect(page).to have_content()
+  end
+end
