bb4413cfe2adfe083a52f19274076f30da6f90ad
kevn7n <kevn_nguyen@yahoo.com>
2021-05-12 23:28:00 -0600

    Update shop feature tests

--- spec/features/shops/show_spec.rb
+++ spec/features/shops/show_spec.rb
@@ -5,12 +5,12 @@ RSpec.describe 'the shop show page', type: :feature do
     @shop_1 = Shop.create!(name: 'Stickys Sweets', sells_drinks: true, varieties: 110)
     @shop_2 = Shop.create!(name: 'Candy Corral', sells_drinks: false, varieties: 45)
 
-    visit "/shops/#{@shop_1.id}"
+      visit "/shops/#{@shop_1.id}"
 
-    expect(page).to have_content("SHOP ##{@shop_1.id}")
     expect(page).to have_content(@shop_1.name)
-    expect(page).to have_content("Sell drinks? -- #{@shop_1.sells_drinks}")
-    expect(page).to have_content("How many candy varieties? -- #{@shop_1.varieties}")
+    expect(page).to have_content("Shop ID#: #{@shop_1.id}")
+    expect(page).to have_content("Sells drinks?: #{@shop_1.sells_drinks}")
+    expect(page).to have_content("How many candy varieties?: #{@shop_1.varieties}")
     expect(page).to_not have_content(@shop_2.name)
 
     # expect(page).to have_link("Edit Discount")
