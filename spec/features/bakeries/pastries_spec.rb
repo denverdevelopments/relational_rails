4908dcd46484f198c6b0dcd19980a8e457b01eac
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-05-19 15:26:08 -0700

    Create context blocks

--- spec/features/bakeries/pastries_spec.rb
+++ spec/features/bakeries/pastries_spec.rb
@@ -14,59 +14,67 @@ RSpec.describe 'the bakery and its pastries' do
     @pastry_5 = @bakery_3.pastries.create!(name: "Rollin in the Cardamom rolls", savory_pastry: false, calories: 600)
   end
 
-  it 'can show the pastries and their attributes associated with bakery' do
-    visit "/bakeries/#{@bakery_1.id}/pastries"
+  context 'index links' do
+    it 'has a link to the pastries index' do
+      visit "/bakeries/#{@bakery_1.id}/pastries"
 
-    expect(page).to have_content(@bakery_1.name)
-    expect(page).to have_content(@pastry_1.name)
-    expect(page).to have_content(@pastry_1.savory_pastry?)
-    expect(page).to have_content(@pastry_1.calories)
-    expect(page).to_not have_content(@pastry_2.name)
+      expect(page).to have_content("Delicious Pastries")
+
+      click_link 'Delicious Pastries'
+
+      expect(current_path).to eq('/pastries')
+
+      visit "/bakeries/#{@bakery_2.id}/pastries"
+
+      expect(page).to have_content("Delicious Pastries")
+
+      click_link 'Delicious Pastries'
+
+      expect(current_path).to eq('/pastries')
+    end
+
+    it 'has a link to the bakeries index' do
+      visit "/bakeries/#{@bakery_1.id}/pastries"
+
+      expect(page).to have_content("Radical Bakeries")
+
+      click_link 'Radical Bakeries'
+
+      expect(current_path).to eq('/bakeries')
+
+      visit "/bakeries/#{@bakery_2.id}/pastries"
+
+      expect(page).to have_content("Radical Bakeries")
+
+      click_link 'Radical Bakeries'
+
+      expect(current_path).to eq('/bakeries')
+    end
   end
 
-  it 'has a link to the pastires index' do
-    visit "/bakeries/#{@bakery_1.id}/pastries"
+  context 'bakery name and their pastries' do
+    it 'can show the pastries and their attributes associated with bakery' do
+      visit "/bakeries/#{@bakery_1.id}/pastries"
 
-    expect(page).to have_content("Delicious Pastries")
-
-    click_link 'Delicious Pastries'
-
-    expect(current_path).to eq('/pastries')
-
-    visit "/bakeries/#{@bakery_2.id}/pastries"
-
-    expect(page).to have_content("Delicious Pastries")
-
-    click_link 'Delicious Pastries'
-
-    expect(current_path).to eq('/pastries')
+      expect(page).to have_content(@bakery_1.name)
+      expect(page).to have_content(@pastry_1.name)
+      expect(page).to have_content(@pastry_1.savory_pastry?)
+      expect(page).to have_content(@pastry_1.calories)
+      expect(page).to_not have_content(@pastry_2.name)
+    end
   end
 
-  it 'has a link to the bakeries index' do
-    visit "/bakeries/#{@bakery_1.id}/pastries"
+  context 'new pastry link' do
+    it 'has a link to create a new pastry' do
+      visit "/bakeries/#{@bakery_2.id}/pastries"
 
-    expect(page).to have_content("Radical Bakeries")
+      expect(page).to have_link("New Pastry")
 
-    click_link 'Radical Bakeries'
+      click_link 'New Pastry'
 
-    expect(current_path).to eq('/bakeries')
-
-    visit "/bakeries/#{@bakery_2.id}/pastries"
-
-    expect(page).to have_content("Radical Bakeries")
-
-    click_link 'Radical Bakeries'
-
-    expect(current_path).to eq('/bakeries')
+      expect(current_path).to eq("/bakeries/#{@bakery_2.id}/pastries/new")
+    end
   end
 
-  it 'has a link to create a new pastry' do
-    visit "/bakeries/#{@bakery_2.id}/pastries"
 
-    expect(page).to have_link("New Pastry")
-
-    click_link 'New Pastry'
-
-    expect(current_path).to eq("/bakeries/#{@bakery_2.id}/pastries/new")
-  end
 end
